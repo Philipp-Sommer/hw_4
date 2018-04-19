8696254a4d413f5eae08c64edc92180161c11fd9
Philipp Sommer <philipp.sommer@edu.uni-graz.at>
2018-04-19 07:24:18 +0200

    initial commit

---
+++ telephone_numbers.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""
+This module contains a function to decode telephone numbers.
+
+.. moduleauthor:: Clemens Eichberger <Clemens.Eichberger@edu.uni-graz.at>
+.. moduleauthor:: Philipp Sommer <Philipp.Sommer@edu.uni-graz.at>
+.. moduleauthor:: Theresa Trinker <Theresa.Trinker@edu.uni-graz.at>
+"""
