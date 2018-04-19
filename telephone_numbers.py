526a7566e163d97360f449fb81529ff0ea885c0f
Philipp Sommer <philipp.sommer@edu.uni-graz.at>
2018-04-19 07:25:32 +0200

    initial commit

--- telephone_numbers.py
+++ telephone_numbers.py
@@ -7,3 +7,9 @@ This module contains a function to decode telephone numbers.
 .. moduleauthor:: Philipp Sommer <Philipp.Sommer@edu.uni-graz.at>
 .. moduleauthor:: Theresa Trinker <Theresa.Trinker@edu.uni-graz.at>
 """
+
+def as_numeric(text):
+    """
+    This function decodes telephone numbers.
+    """
+    d = {'a': 2, 'b': 2, 'c': 2, }  # finish this somewhen
